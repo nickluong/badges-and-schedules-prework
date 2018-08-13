e9e6f65781b8bd5e98d63553b6b60724105b7ae7
Nicholas  Luong <nckluong@gmail.com>
2018-08-13 23:58:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(arr)
+  badgeArr = [] 
+  arr.each do |name|
+    badgeArr << badge_maker(name)
+  end 
+  badgeArr
+end 
+
+def assign_rooms(arr)
+  assignArr = [] 
+  count = 0 
+  until count == 7
+    assignArr << "Hello, #{arr[count]}! You'll be assigned to room #{count+1}!"
+    count += 1 
+  end 
+  assignArr
+end 
+
+def printer(attendees)
+  count = 0 
+  badgeArr = batch_badge_creator(attendees)
+  assignArr = assign_rooms(attendees)
+  until count == attendees.size-1 
+    puts badgeArr[count]
+    puts assignArr[count]
+  end
+end 
